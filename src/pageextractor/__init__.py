abcca67ce72dab3c3d92afb000c68d38eba1db3b
Herbert Kruitbosch <H.T.Kruitbosch@rug.nl>
2025-10-03 12:28:25 +0200

    fourcorner estimation bug+alternative crops added

--- src/pageextractor/__init__.py
+++ src/pageextractor/__init__.py
@@ -1,5 +1,5 @@
 import json
-import shapely
+import shapely.ops
 import skimage
 import rasterio.features
 from tqdm.cli import tqdm
@@ -14,6 +14,18 @@ from hydra.utils import instantiate
 from transformers import AutoModelForZeroShotObjectDetection, AutoProcessor
 from sam2.sam2_image_predictor import SAM2ImagePredictor
 
+def convexify(polygon):
+  """Removes points from the polygon that are far from the convex hull, i.e.
+  sqrt(area)/100 as a max dist, again pulled out of shy bass"""
+  conv_hull = shapely.convex_hull(polygon)
+  dmax = np.sqrt(polygon.area) / 100
+  simplified = shapely.make_valid(shapely.Polygon([
+      [x,y] for x, y in zip(*polygon.exterior.xy)
+      if shapely.Point(x, y).distance(conv_hull.exterior) < dmax
+  ]))
+  if hasattr(simplified, 'geoms'):
+    return max(simplified.geoms, key=lambda x: x.area)
+  return simplified
 
 def simplify_to_n(polygon, n, eps=1):
   """Simplifies the polygon to have n vertices
@@ -22,39 +34,65 @@ def simplify_to_n(polygon, n, eps=1):
   resulting in a n+1 vertice polygon. E.g. causes more binary search
   iterations but also a tighter fit."""
   assert n >= 3
-  
-  #remove holes
-  polygon = shapely.Polygon(np.array(polygon.exterior.coords.xy).T)
-  tolerance = np.sqrt(polygon.area) # not really well thought through start
-  # [0, inf) interval, algorithm will reduce this interval through binary
-  # search
-  min_tolerance = 0
-  max_tolerance = None
-  simplified = shapely.simplify(polygon, tolerance=tolerance)
-  coords = simplified.exterior.coords.xy
-  # n+1 because first and last coord are the same, i.e. closed polygon.
-  while len(coords[0]) != n+1 or (max_tolerance - min_tolerance) > eps:
-    # too coarse, reduce tolerance
-    # `<=` is important, when the number of vertices is correct,
-    # you want to find the lowest tolerance that wouldn't cause to n+1
-    # vertices, e.g. we choose the `==` part to be the branch that
-    # lowers the tolerance. Otherwise we'd find the hightest tolerance
-    # that would still lead to n vertices instead of `n-1`.
-    # Moreover, n-1 isn't always possible, i.e. if someone wants a
-    # triangle and sets n=3, `simplify` will never return a line (I think).
-    if len(coords[0]) <= n+1:
-      max_tolerance = tolerance
-      tolerance = (min_tolerance + max_tolerance) / 2
-    else:
-      # too precise, increase tolerance
-      min_tolerance = tolerance
-      if max_tolerance is None:
-        tolerance *= 2 # just double if no maximum found yet
-      else:
-        tolerance = (min_tolerance + max_tolerance) / 2
+
+  convexified = False
+
+  def f(polygon, n, eps):
+    nonlocal convexified
+    #remove holes
+    polygon = shapely.Polygon(np.array(polygon.exterior.coords.xy).T)
+    tolerance = np.sqrt(polygon.area) # not really well thought through start
+    # [0, inf) interval, algorithm will reduce this interval through binary
+    # search
+    min_tolerance = 0
+    max_tolerance = None
+
+    # sometimes the algorithm can explode into endlessly increasing the tolerance,
+    # yet the polygon won't reduce in points because of weildy, non-convex
+    # convavities. E.g. something like this:
+    # |‾‾‾‾‾‾‾|
+    # |       |
+    # |_||    |
+    # |‾‾     |
+    # |       |
+    # |_______|
+    # this caps the tolerance to the square root of the area, e.g. a
+    # rough estimate of a number that I pulled out of shy bass for the something
+    # relative to the circumference of a shape with that area.
+    max_min_tolerance = np.sqrt(polygon.area)
+
     simplified = shapely.simplify(polygon, tolerance=tolerance)
     coords = simplified.exterior.coords.xy
-  return simplified
+    # n+1 because first and last coord are the same, i.e. closed polygon.
+    while len(coords[0]) != n+1 or (max_tolerance - min_tolerance) > eps:
+      if min_tolerance > max_min_tolerance:
+        if convexified:
+          raise ValueError(f"Failed to simplify at tolerance [{min_tolerance}, inf), probably weirdly non-convex")
+        convexified = True
+        return simplify_to_n(convexify(polygon), n=n, eps=eps)
+      # too coarse, reduce tolerance
+      # `<=` is important, when the number of vertices is correct,
+      # you want to find the lowest tolerance that wouldn't cause to n+1
+      # vertices, e.g. we choose the `==` part to be the branch that
+      # lowers the tolerance. Otherwise we'd find the hightest tolerance
+      # that would still lead to n vertices instead of `n-1`.
+      # Moreover, n-1 isn't always possible, i.e. if someone wants a
+      # triangle and sets n=3, `simplify` will never return a line (I think).
+      if len(coords[0]) <= n+1:
+        max_tolerance = tolerance
+        tolerance = (min_tolerance + max_tolerance) / 2
+      else:
+        # too precise, increase tolerance
+        min_tolerance = tolerance
+        if max_tolerance is None:
+          tolerance *= 2 # just double if no maximum found yet
+        else:
+          tolerance = (min_tolerance + max_tolerance) / 2
+      simplified = shapely.simplify(polygon, tolerance=tolerance)
+      coords = simplified.exterior.coords.xy
+    return simplified
+  return f(polygon, n, eps)
+
 
 def aligned_rectangle_coords(polygon):
   """Aligns shapely.Polygon's boundary to clockwise oriantation
@@ -130,11 +168,13 @@ class PageExtractor():
     )
 
     self.predictor.set_image(np.array(page))
-    masks, scores, logits = self.predictor.predict(
-        box=results[0]['boxes'], multimask_output=False)
+    masks = self.predictor.predict(
+        box=results[0]['boxes'], multimask_output=False)[0] #[0] for masks
     if len(masks.shape) > 3:
       masks = np.squeeze(masks, axis=1)
     mask = masks[0]
+
+    background_white = np.maximum((255-255*mask[..., None]).astype(np.uint8), np.array(page))
 
     maskShape = list(rasterio.features.shapes(mask.astype('uint8')))
     polygon = max( # extract largest area with mask==1, assume this is the page
@@ -143,17 +183,54 @@ class PageExtractor():
         for shape, v in maskShape if v == 1
       ), key=lambda x: x.area
     )
-    polygon = simplify_to_n(polygon, 4)
-    fourcorner = aligned_rectangle_coords(polygon)
-    h, w = map(int, np.sqrt(((fourcorner - fourcorner[[1,2,3,0]]) ** 2).sum(1)).reshape(2,2).mean(0).round())
+    # remove holes
+    polygon = shapely.Polygon(np.stack(polygon.exterior.xy).astype(int).T)
+    # fourcorner to map the polygon from or snap the polygon to, e.g. angles
+    # might be skewed
+    fourcorner = simplify_to_n(polygon, 4)
+    if shapely.is_ccw(fourcorner):
+      fourcorner = shapely.reverse(fourcorner)
+    fourcorner_pts = aligned_rectangle_coords(fourcorner)
+
+
+    #box to map stuff to, this is actually a box with 90deg angles.
+    # h, w as the average of the 4-corner edge lengths
+    h, w = map(int, np.sqrt(((fourcorner_pts - fourcorner_pts[[1,2,3,0]]) ** 2).sum(1)).reshape(2,2).mean(0).round())
     src = np.array([[0, 0], [0, h], [w, h], [w, 0]])
-    
-    tform3 = skimage.transform.ProjectiveTransform()
-    tform3.estimate(src, fourcorner)
-    cropped = skimage.transform.warp(np.array(page), tform3, output_shape=(h, w))
+
+    # simplified polygon, otherwise the stretching becomes hella expensive
+    simplified = shapely.simplify(polygon, tolerance=np.sqrt(polygon.area)/300)
+
+    # snap the polygon to the 4 corners, e.g. a Nx2x2 array of
+    # N polygon points, with the original and snapped point. E.g. `mapping[:,0]`
+    # are all points on the polygon and `mapping[:, 1]` are the related points
+    # on the fourcorner that are closest
+    mapping = np.array([
+        [[p.x, p.y] for p in shapely.ops.nearest_points(shapely.Point(x, y), fourcorner.exterior)]
+        for x, y in zip(*simplified.exterior.xy)
+    ]).astype(int)
+
+    tform0 = skimage.transform.PiecewiseAffineTransform()
+    tform0.estimate(mapping[:, 1], mapping[:, 0])
+
+    tform1 = skimage.transform.ProjectiveTransform()
+    tform1.estimate(src, fourcorner_pts)
+
+    def chain_tform(t0, t1):
+      trans = lambda pts: t0(t1(pts))
+      trans.inverse = lambda pts: t1.inverse(t0.inverse(pts))
+      return trans
+
+    # only crop the fourcorners and project to a rectangle.
+    cropped = skimage.transform.warp(np.array(page), tform1, output_shape=(h, w))
     cropped = (255 * cropped).astype(np.uint8)
     cropped = Image.fromarray(cropped)
-    return mask, fourcorner, cropped
+
+    # Alternative 'crop': piecewice affine transform to fourcorners, then crop and
+    # project to a rectangle as before. This maps or stretches curved pages inside the crop.
+    page_corrections = skimage.transform(np.array(page), chain_tform(tform0, tform1), output_shape=(h, w))
+
+    return mask, fourcorner, polygon, background_white, cropped, page_corrections
 
   def extract_pages(self, pages, prompt=None):
     """Extracts pages from photo's of pages using SAM.
@@ -161,9 +238,12 @@ class PageExtractor():
 
     prompt = prompt if prompt is not None else self.text_prompt
     return [
-        {'mask': mask, 'cropped': cropped, 'polygon': fourcorner}
+        {'mask': mask, 'cropped': cropped, 'polygon': fourcorner, 'polygon': polygon,
+         'white_background': background_white, 'page_corrections': page_corrections}
         for page in tqdm(pages)
-        for mask, fourcorner, cropped in [self.extract_page(page, prompt)]
+        for mask, fourcorner, polygon, background_white, cropped, page_corrections in [
+          self.extract_page(page, prompt)
+        ]
     ]
 
   def unload(self):
